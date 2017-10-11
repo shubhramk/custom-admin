e15ae77bda896d5a109675094e33a0050e9727c7
skrishna <shubhram.krishna@mlb.com>
2017-10-11 09:01:32 +0530

    datatable added

--- src/app/common/component/datatable/datatable.component.ts
+++ src/app/common/component/datatable/datatable.component.ts
@@ -1,30 +1,132 @@
-import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import {
+  Component, Input, HostListener, EventEmitter, Output, SimpleChanges, OnChanges,
+  AfterViewInit, OnInit
+} from '@angular/core';
 
 import 'datatables.net';
 import $ from 'jquery';
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-datatable',
-  templateUrl: './datatable.component.html',
-  styleUrls: ['./datatable.component.css']
+  template: `
+       <div class="custom-dt-table">
+          <table [attr.id]="elemID" class="dt-table table table-bordered table-striped" style="width:100%;"></table>
+        </div>
+    `
 })
-export class DatatableComponent implements AfterViewInit {
-   elemID = 'tab' + Math.random().toString(36).slice(2);
-   dt:any;
-   @Input()
-   dtTableData:any;
-   dtTableHeader:any;
+export class DatatableComponent implements OnChanges, AfterViewInit, OnInit {
+  @Input()
+  dtTableData:any;
+  dtTableHeader:any;
+
+  @Input() config;
+  @Input() reload;
+  @Input() data;
+  @Output() onMenuSelect: EventEmitter<any> = new EventEmitter();
+
+  elemID = 'dt-' + Math.random().toString(36).slice(2);
+  dt: any = null;
+  COMMON_CONFIG: Object = {};
+
+  //Constructor
   constructor() { }
 
+  //on component init
+  ngOnInit() {
+    this.initConfig();
+  }
+
+  //when data changes
+  ngOnChanges(changes: SimpleChanges) {
+
+    let reloadDT = changes['reload'];
+    let data = changes['data'] || [];
+
+
+    if (reloadDT) {
+      if (data['currentValue'] != data['previousValue']) {
+        setTimeout(() => this.reloadTable(), 500);
+      }
+    }
+
+    if (data) {
+      if (data['currentValue'] != data['previousValue']) {
+        setTimeout(() => this.addData(), 100);
+      }
+    }
+  }
+  //called after view is initialized
   ngAfterViewInit() {
-    
-    if(this.dt){
-      this.dtTableHeader = [];
+    //hiding data table errors
+    //$.fn.dataTable.ext.errMode = 'none';
+
+    //merging common options and user options
+    let options: Object = _.extend(
+      {},
+      this.COMMON_CONFIG,
+      this.config
+    );
+    this.initDT(options);
+  }
+
+  //init data table
+  initDT(options: Object): void {
+    if (this.dt) {
       this.dt.clear().destroy();
       this.dt = null;
+      setTimeout(() => this.initDT(options), 0);
+    } else {
+      this.dt = $('#' + this.elemID).DataTable(options);
+      setTimeout(() => this.onInitComplete(), 200);
     }
-      this.dtTableHeader = Object.keys(this.dtTableData[0]);
-     // this.dtTableHeader.pop();
-      this.dt = $('#'+this.elemID).DataTable();
+  }
+
+  //reload table
+  reloadTable(): void {
+    if (this.dt) {
+      //reload logic
+    }
+  }
+
+  //add data through JSON / Array
+  addData() {
+    let options: Object = _.extend(
+      {},
+      this.COMMON_CONFIG,
+      this.config,
+      { "data": this.data || [] }
+    );
+    this.initDT(options);
+  }
+
+  //init data Table common config
+  initConfig() {
+    this.COMMON_CONFIG = {
+       displayLength: 10,
+       destroy: false,
+       order: [],
+       lengthMenu: [[10, 20, 25], [10, 20, 25]],
+       data: []
+    };
+  }
+
+  //binding event on data table init
+  onInitComplete() {
+    let self = this;
+    let table = $('#' + self.elemID + "_wrapper .dt-table").DataTable();
+
+    // menu functionality
+    $('#' + self.elemID + "_wrapper .dt-table").off('click', 'a');
+    $('#' + self.elemID + "_wrapper .dt-table").on('click', 'a', function () {
+      self.onMenuClicked({ 'clickedOn': $(this).attr('data-name'), "value": $(this).attr('data-custom') });
+    });
+
+  }
+
+  //on menu event bind ie. edit/add/delete icons
+  onMenuClicked(obj: any): void {
+    this.onMenuSelect.emit(obj);
   }
 
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,32 +1,70 @@
-import {Component, AfterViewInit, ElementRef} from '@angular/core';
-import Chartist from 'chartist'; 
+import {Component, AfterViewInit, ElementRef, OnInit} from '@angular/core';
+//import Chartist from 'chartist';
 @Component({
   styleUrls: ['./home.component.scss'],
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements AfterViewInit{
-  visitData = [{total:2301, name:"Total Visits", symbol:"down", color:'red', val:[ 0, 5, 6, 10, 9, 12, 4, 9]}, 
+export class HomeComponent implements OnInit,AfterViewInit{
+  visitData = [{total:2301, name:"Total Visits", symbol:"down", color:'red', val:[ 0, 5, 6, 10, 9, 12, 4, 9]},
               {total:2421, name:"Page Views", symbol:"up", color:'blue', val:[ 0, 5, 6, 10, 9, 12, 4, 9]},
               {total:2515, name:"Unique Visits", symbol:"down", color:'yellow', val:[ 0, 5, 6, 10, 9, 12, 4, 9]},
               {total:1122, name:"Bounce Rate", symbol:"up", color:'green', val:[ 0, 5, 6, 10, 9, 12, 4, 9]}
   ];
-    topGeneralSh8ke = 
-    [
-      {Title:"goals", Description:"Share, Socialize", Category:"goals", "Times sh8ken":167, "Times sh8red":0, Creater:"akshay Kumar"},
-      {Title:"goals", Description:"Share, Socialize", Category:"goals", "Times sh8ken":167, "Times sh8red":0, Creater:"akshay Kumar"},
-      {Title:"goals", Description:"Share, Socialize", Category:"goals", "Times sh8ken":167, "Times sh8red":0, Creater:"akshay Kumar"},
-      {Title:"goals", Description:"Share, Socialize", Category:"goals", "Times sh8ken":167, "Times sh8red":0, Creater:"akshay Kumar"},
-      {Title:"goals", Description:"Share, Socialize", Category:"goals", "Times sh8ken":167, "Times sh8red":0, Creater:"akshay Kumar"},
-      {Title:"goals", Description:"Share, Socialize", Category:"goals", "Times sh8ken":167, "Times sh8red":0, Creater:"akshay Kumar"}
-    ];
+  topGeneralSh8ke =[];
   initialCount:number = 30;
-  constructor(private elRef:ElementRef) {
+  dtConfig:Object = {};
+  constructor() {}
 
+  ngOnInit(){
+    this.dtConfig = {
+      "columnDefs": [
+        {
+          "targets": 6,
+          "width": "10%",
+          "orderable": false,
+          "className": "noPadding",
+          "render": function (data, type, full, meta) {
+            var template = '';
+
+            let val = data;
+            template = '<div class="dt-menu-icons">' +
+              '<a href="javascript:void(0);" data-name="edit" data-custom="' + val + '"><span class="fa fa-pencil" aria-hidden="true"></span></a>' +
+              '<a href="javascript:void(0);" data-name="delete" data-custom="' + val + '"><span class="fa fa-trash-o" aria-hidden="true"></span></a>' +
+              '</div>';
+
+            return template;
+          }
+        }
+      ],
+      "columns": [
+        { "title": 'Title', "data": "title" },
+        { "title": 'Description', "data": "description" },
+        { "title": 'Category', "data": "category" },
+        { "title": 'Times sh8ken', "data": "timesh8ken" },
+        { "title": 'Times sh8red', "data": "timesh8red" },
+        { "title": 'Creater' , "data":"creater"},
+        { "title": '' , "data":"title"}
+      ]
+    }
+
+    //data
+    this.topGeneralSh8ke = [
+      {title:"goals", description:"Share, Socialize", category:"goals 1", "timesh8ken":167, "timesh8red":0, creater:"akshay Kumar"},
+      {title:"goals", description:"Share, Socialize", category:"goals 2", "timesh8ken":167, "timesh8red":0, creater:"akshay Kumar"},
+      {title:"goals", description:"Share, Socialize", category:"goals 3", "timesh8ken":167, "timesh8red":0, creater:"akshay Kumar"},
+      {title:"goals", description:"Share, Socialize", category:"goals 4", "timesh8ken":167, "timesh8red":0, creater:"akshay Kumar"},
+      {title:"goals", description:"Share, Socialize", category:"goals 5", "timesh8ken":167, "timesh8red":0, creater:"akshay Kumar"},
+      {title:"goals", description:"Share, Socialize", category:"goals 6", "timesh8ken":167, "timesh8red":0, creater:"akshay Kumar"}
+      ];
   }
 
-  ngAfterViewInit(){
-    
-        
-
+  //on Menu Icon selected
+  onMenuSelect(data: any) {
+    if (data['clickedOn'] == 'edit') {
+      let customData = data['value'];
+      alert(customData);
+    }
   }
+
+  ngAfterViewInit(){}
 }
